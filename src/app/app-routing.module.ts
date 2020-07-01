cb6be64c1a4cd3c432829dae108ea0b9bbdd284d
Sai Charan Allam <sai@rossum.io>
2020-07-01 15:35:43 +0530

    tutorial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MobileDashboardComponent } from './mobile-dashboard/mobile-dashboard.component';
+import { MobileViewComponent } from './mobile-view/mobile-view.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "mobiledashboard", component: MobileDashboardComponent },
+  { path: "mobileview", component: MobileViewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MobileDashboardComponent } from './mobile-dashboard/mobile-dashboard.component';
+import { MobileViewComponent } from './mobile-view/mobile-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MobileDashboardComponent,
+    MobileViewComponent
   ],
   imports: [
     BrowserModule,
